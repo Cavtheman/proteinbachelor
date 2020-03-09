3fd5d3b4e7048ad5327a10270bf34257c66b261b
Cavtheman <casper_lf@yahoo.dk>
2020-03-09 18:04:44 +0100

    wrote the base code for training the model and changed data_generator to also generate target labels. Loss function doesnt like the dimensions though

--- data_generator.py
+++ data_generator.py
@@ -4,16 +4,15 @@ import numpy as np
 import torch
 
 class data_generator():
-    def __init__(self, filename, max_seq_len, acid_dict={}):
-        self.__acids__ = "ACDEFGHIKLMNPQRSTVWY-"
+    def __init__(self, filename, max_seq_len, acids="ACDEFGHIKLMNPQRSTUVWY-"):
+        self.__acids__ = acids
         self.__parser__ = SeqIO.parse(filename, "fasta")
 
-        self.acid_dict = acid_dict
+        self.acid_dict = {}
         self.max_seq_len = max_seq_len
         self.data = torch.Tensor(1, self.max_seq_len, len(self.acid_dict))
-
-        if (acid_dict == {}):
-            self.gen_acid_dict()
+        self.targets = torch.Tensor(1, self.max_seq_len, len(self.acid_dict))
+        self.gen_acid_dict(acids)
 
     # If a sequence contains one of XBZJ it will be discarded,
     # and if it is longer than the given max_seq_length
@@ -23,9 +22,9 @@ class data_generator():
         return len_val and cont_val
 
     # Generate a dictionary if none is supplied,
-    def gen_acid_dict(self):
-        for i, elem in enumerate(self.__acids__):
-            temp = np.zeros(len(self.__acids__))
+    def gen_acid_dict(self, acids):
+        for i, elem in enumerate(acids):
+            temp = np.zeros(len(acids))
             temp[i] = 1
             self.acid_dict[elem] = temp
         return self.acid_dict
@@ -33,6 +32,7 @@ class data_generator():
     # Read num_elems sequences from the given file
     def gen_data(self, num_elems):
         ret_val = []
+        targets = []
         i = 0
         for record in self.__parser__:
             if (i == num_elems):
@@ -40,11 +40,18 @@ class data_generator():
             elif (not self.__is_legal_seq__(record.seq)):
                 continue
             else:
-                temp_full = np.full((self.max_seq_len - len(record), len(self.acid_dict)), self.acid_dict['-'])
-                temp = np.array([self.acid_dict[elem] for elem in record])
-                ret_val.append(torch.tensor(np.concatenate((temp, temp_full), axis=0), dtype=torch.long))
+                pad_vals = np.full((self.max_seq_len - len(record), len(self.acid_dict)), self.acid_dict['-'])
+                seq_vals = np.array([self.acid_dict[elem] for elem in record])
+                temp_target_vals = [self.acid_dict[elem] for elem in record]
+                temp_target_vals.append(self.acid_dict['-'])
+                target_vals = np.array(temp_target_vals[1:])
+
+                ret_val.append(torch.tensor(np.concatenate((seq_vals, pad_vals), axis=0), dtype=torch.float))
+                targets.append(torch.tensor(np.concatenate((target_vals, pad_vals), axis=0), dtype=torch.float))
                 i += 1
+
         self.data = torch.stack(ret_val)
+        self.targets = torch.stack(targets)
         #self.data = torch.Tensor(num_elems, self.max_seq_len, len(self.acid_dict))
         #torch.cat(ret_val, out=self.data)
 
