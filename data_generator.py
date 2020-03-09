e735d8636586d1f48e79465c714faa9b069d530b
Cavtheman <casper_lf@yahoo.dk>
2020-03-09 12:40:17 +0100

    fixed the dictionary

--- data_generator.py
+++ data_generator.py
@@ -1,17 +1,17 @@
 from Bio import SeqIO
 from string import ascii_uppercase
-
+import numpy as np
 
 class data_generator():
     def __init__(self, filename, max_seq_length, acid_dict={}):
-        if (acid_dict = {}):
-            self.acid_dict = self.gen_acid_dict()
-        else:
-            self.acid_dict = acid_dict
+        self.acid_dict = acid_dict
+        self.__acids__ = "ACDEFGHIKLMNPQRSTVWY-"
         self.data = []
         self.max_seq_length = max_seq_length
         self.__parser__ = SeqIO.parse(filename, "fasta")
-        self.__acids__ = "ACDEFGHIKLMNPQRSTVWY-"
+
+        if (acid_dict == {}):
+            self.gen_acid_dict()
 
     # If a sequence contains one of XBZJ it will be discarded,
     # and if it is longer than the given max_seq_length
@@ -38,3 +38,13 @@ class data_generator():
                 continue
             else:
                 self.data.append(str(record.seq).upper())
+
+'''
+large_file = "uniref50.fasta"
+small_file = "100k_rows.fasta"
+
+data_gen = data_generator(large_file, 2000)
+data_gen.gen_data(1000)
+print(data_gen.acid_dict['B'])
+#print(data_gen.data[0])
+'''
