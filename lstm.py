dbd132288c916de4213d906543de3063de539ea9
Cavtheman <casper_lf@yahoo.dk>
2020-04-27 17:32:34 +0200

    latest updates and working simple lstm

--- lstm.py
+++ lstm.py
@@ -8,11 +8,12 @@ class LSTM_model(nn.Module):
         self.hidden_layer_size = hidden_layer_size
         self.nr_hidden_layers = nr_hidden_layers
 
-        self.model = nn.LSTM(input_size, hidden_layer_size, nr_hidden_layers, batch_first=True)
+        self.model = nn.LSTM(input_size, hidden_layer_size, nr_hidden_layers, batch_first=False)
 
         self.linear = nn.Linear(hidden_layer_size, input_size)
 
     def forward(self, input_data):
+
         lstm_out, (hn, cn) = self.model(input_data)
         #print("LSTM out:", lstm_out.size())
         tag_space = self.linear(lstm_out)
@@ -20,7 +21,7 @@ class LSTM_model(nn.Module):
         #tag_scores = F.log_softmax(tag_space, dim=1)
         #print(type(tag_scores))
         #output = torch.argmax(tag_space, dim=2).float()
-        return tag_space, (hn, cn)
+        return tag_space, lstm_out
 
 class LSTMCell(nn.Module):
     def __init__(self, feature_size, hidden_layer_size, nr_hidden_layers, max_seq_len):
@@ -36,15 +37,14 @@ class LSTMCell(nn.Module):
 
 
     def init_hidden(self, batch_size):
-        return (torch.zeros(batch_size, self.hidden_layer_size),
-                torch.zeros(batch_size, self.hidden_layer_size))
+        return (torch.randn(batch_size, self.hidden_layer_size),
+                torch.randn(batch_size, self.hidden_layer_size))
 
 
     def forward(self, input_data, hidden):
-        hn, cn = hidden
         #print(hn, cn)
-        hn, cn = self.model(input_data, (hn, cn))
+        lstm_out, _ = self.model(input_data, hidden)
         #print(hn, cn)
-        output = self.linear(hn)
+        output = self.linear(lstm_out)
         #print(output)
-        return output, (hn,cn)
+        return output, lstm_out
