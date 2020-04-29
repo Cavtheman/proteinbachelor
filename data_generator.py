866373d512684162890089cdcdf4ee536a78a553
Cavtheman <casper_lf@yahoo.dk>
2020-04-29 17:19:19 +0200

    more extensive documentation for the classes in separate files

--- data_generator.py
+++ data_generator.py
@@ -9,13 +9,38 @@ import torch.nn.functional as F
 
 
 class Dataset(data.Dataset):
-    # Checks whether a given sequence is legal
+    '''
+    Checks whether a given sequence is legal
+
+    Parameters
+    ----------
+    seq : String
+        Raw unprocessed string from the fasta file
+
+    Returns
+    ----------
+    Bool
+    '''
     def __is_legal_seq__(self, seq):
         len_val = not (len(seq) > self.max_seq_len)
         cont_val = not(('X' in seq) or ('B' in seq) or ('Z' in seq) or ('J' in seq))
         return len_val and cont_val
 
-    # Generates a dictionary given a string with all the elements
+    '''
+    Generates a dictionary given a string with all the elements that will be in the dictionary
+
+    Parameters
+    ----------
+    acids : String
+        An "alphabet" to generate the dictionary from.
+        The last char will be used as a padding value
+
+    Returns
+    ----------
+    acid_dict : Dictionary of Tensors
+        A dictionary with the same length as the input string.
+        It is effectively a one-hot encoding of acids
+    '''
     def __gen_acid_dict__(self, acids):
         acid_dict = {}
         for i, elem in enumerate(acids):
@@ -25,6 +50,25 @@ class Dataset(data.Dataset):
             #print(elem, "=", acid_dict[elem])
         return acid_dict
 
+    '''
+    Initialisation for the Dataset
+
+    Parameters
+    ----------
+    filename : String
+        Path to a fasta file with data.
+    max_seq_len : Int
+        An integer representing the longest sequences we want to take into account.
+    acids : String
+        An "alphabet" to generate the dictionary from.
+
+    Variables
+    ----------
+    acid_dict : Dictionary of Tensors
+        See __gen_acid_dict__
+    data : List of Strings
+        The entire input file loaded as strings
+    '''
     def __init__(self, filename, max_seq_len, acids="ACDEFGHIKLMNPQRSTVWY-"):
         elem_list = []
         self.acids = acids
@@ -37,19 +81,56 @@ class Dataset(data.Dataset):
                 elem_list.append(elem.seq)
         self.data = elem_list
 
+    '''
+    Method to get the length of the dataset
+
+    Returns
+    ----------
+    Int : Length of the entire dataset
+    '''
     def __len__(self):
         return len(self.data)
 
+    '''
+    Preprocesses a sequence into something usable by an LSTM
+
+    Parameters
+    ----------
+    seq : String
+        The sequence to be prepared
+
+    Returns
+    ----------
+    tensor_seq : Tensor of size max_seq_len x len(acid_dict)
+        The padded, preprocessed Tensor of one-hot encoded acids
+    labels_seq : Tensor of size max_seq_len
+        Contains the labels for each element in tensor_seq
+        as the correct index of the one-hot encoding
+    valid_elems : Int
+        Integer value representing the length of the sequence before padding
+    '''
     def __prepare_seq__(self, seq):
         valid_elems = min(len(seq)+1, self.max_seq_len)
-        seq = str(seq).ljust(self.max_seq_len+1, '-')
+        seq = str(seq).ljust(self.max_seq_len+1, self.acids[-1])
         temp_seq = [self.acid_dict[x] for x in seq]
         tensor_seq = torch.stack(temp_seq[:-1], dim=0).float()#.view(self.max_seq_len, 1, -1)
 
         # Labels consisting of the index of correct class
         labels_seq = torch.argmax(torch.stack(temp_seq[1:]), dim=1).long()#.view(-1, 1)
-
         return tensor_seq, labels_seq, valid_elems
 
+    '''
+    Used by the data_generator class to get items
+
+    Parameters
+    ----------
+    index : Int
+        Index to take the data from
+
+    Returns
+    ----------
+    Tuple : See __prepare_seq__
+        A prepared sequence from a specific index in the dataset
+    '''
     def __getitem__(self, index):
         return self.__prepare_seq__(self.data[index])
--- lstm.py
+++ lstm.py
@@ -2,24 +2,68 @@ import torch
 import torch.nn as nn
 import torch.nn.utils.rnn as rnn
 
+
 class LSTM_model(nn.Module):
-    def __init__(self, input_size, hidden_layer_size, nr_hidden_layers, max_seq_len, batch_size):
+    '''
+    Initialises the model for use
+
+    Parameters
+    ----------
+    input_size : Int
+        Length of the "alphabet".
+    hidden_layer_size : Int
+        Size of the hidden layer between the LSTM and the linear layer.
+    nr_hidden_layers: Int
+        Number of LSTMs in sequence.
+    max_seq_len : Int
+        An integer representing the longest sequences we want to take into account.
+    batch_size : Int
+        The number of sequences the model will look at in parallel at any time
+
+    Variables
+    ----------
+    model : nn.LSTM
+        The actual pytorch LSTM to be trained
+    linear : nn.Linear
+        A single linear layer for converting the LSTMs output to the same size as the input
+    '''
+    def __init__(self, input_size, hidden_layer_size, nr_hidden_layers, max_seq_len, batch_size, processor):
         super(LSTM_model, self).__init__()
         self.input_size = input_size
         self.hidden_layer_size = hidden_layer_size
         self.nr_hidden_layers = nr_hidden_layers
         self.max_seq_len = max_seq_len
         self.batch_size = batch_size
+        self.init_hidden = (torch.randn(self.nr_hidden_layers,
+                                        self.batch_size,
+                                        self.hidden_layer_size).to(processor),
+                            torch.randn(self.nr_hidden_layers,
+                                        self.batch_size,
+                                        self.hidden_layer_size).to(processor))
 
         self.model = nn.LSTM(input_size, hidden_layer_size, nr_hidden_layers, batch_first=False)
 
         self.linear = nn.Linear(hidden_layer_size, input_size)
 
+    '''
+    Forward pass of the model
+
+    Parameters
+    ----------
+    input_data : PackedSequence with batch_size batches and input_size features
+        The input data to be predicted/trained on
+
+    Returns
+    ----------
+    tag_space : Tensor of size max_seq_len x batch_size x input_size
+        Padded output of predictions for the sequences
+    lstm_out : PackedSequence with batch_size x hidden_layer_size features
+        Hidden layers of the lstm for all elements in the sequences.
+        Used for dimensionality reduction later to see differences between proteins
+    '''
     def forward(self, input_data):
         #print(input_data.data.size())
-        hidden = (torch.randn(self.nr_hidden_layers, self.batch_size, self.hidden_layer_size),
-                  torch.randn(self.nr_hidden_layers, self.batch_size, self.hidden_layer_size))
-        lstm_out, (hn, cn) = self.model(input_data)
+        lstm_out, (hn, cn) = self.model(input_data, self.init_hidden)
         lin_in, _ = rnn.pad_packed_sequence(lstm_out, total_length=self.max_seq_len)
         #print("LSTM out:", lstm_out.size())
         tag_space = self.linear(lin_in)
@@ -29,6 +73,9 @@ class LSTM_model(nn.Module):
         #output = torch.argmax(tag_space, dim=2).float()
         return tag_space, lstm_out
 
+'''
+Not in use any more
+'''
 class LSTMCell(nn.Module):
     def __init__(self, feature_size, hidden_layer_size, nr_hidden_layers, max_seq_len):
         super(LSTMCell, self).__init__()
